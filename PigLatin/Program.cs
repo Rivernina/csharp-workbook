16e144e06bec41611a8cf6cddebb0fdc2217e0e4
Nina Riveron <Rivernina512@gmail.com>
2018-04-30 18:15:46 -0500

    PigLatin & RockPaperScissors

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -2,20 +2,55 @@
 
 namespace PigLatin
 {
-    class Program
-    {
-        public static void Main()
-        {
-            // your code goes here
 
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
-        }
-        
-        public static string TranslateWord(string word)
-        {
-            // your code goes here
-            return word;
-        }
-    }
+public class Program
+		{
+		public static void Main()
+				{
+						Console.WriteLine("{0,70}", "*****************************************");
+						Console.WriteLine("");
+						Console.WriteLine("{0,65}", "Welcome to PigLatin translation!");
+						Console.WriteLine("");
+
+						Console.WriteLine("{0,70}", "*****************************************");
+						Console.WriteLine("");
+						Console.WriteLine("");
+
+
+
+						Console.WriteLine("Enter a word or phrase that you would like to translate into PigLatin:");
+
+						string input = Console.ReadLine();
+
+						string[] phrase = input.Split(' ');
+
+						foreach (string word in phrase)
+						{
+								Console.WriteLine(TranslateToPig(word));
+						}
+
+						Console.ReadLine();
+						Environment.Exit(0);
+
+				}
+
+		public static string TranslateToPig(string word)
+				{
+						word = word.ToLower();
+						int firstVowelIndex = word.IndexOfAny(new char[] {'e', 'a', 'i', 'o', 'u', 'y'});
+						//Console.WriteLine(firstVowelIndex.ToString());
+
+						if (firstVowelIndex <= 0)
+						{
+								return word + "yay";
+						}
+						else
+						{
+						string firstPart = word.Substring(0, firstVowelIndex);
+						string secondPart = word.Substring(firstVowelIndex);
+
+						return secondPart + firstPart + "ay";
+						}
+				}
+		}
 }
