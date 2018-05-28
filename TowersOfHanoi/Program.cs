93c98f04613457e8042c3083c2702f0422b60c9c
Nina Riveron <Rivernina512@gmail.com>
2018-05-28 17:11:26 -0500

    TOH

--- TowersOfHanoi/Program.cs
+++ TowersOfHanoi/Program.cs
@@ -2,11 +2,34 @@
 
 namespace TowersOfHanoi
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static void Main(string[] args)
+        {   
+            Console.WriteLine("Please enter the number of disks: ");
+            int disks = int.Parse(Console.ReadLine());
+            Tower(disks, "A", "B", "C");
         }
+
+            public static void Tower(int n, string startRod, string auxiliaryRod, string endRod)
+            {
+                if (n == 1)
+                {
+                    Console.WriteLine(startRod + " -> " + endRod);
+                }
+				else if (n < 1)
+				{
+					Console.WriteLine("Error. Please enter valid number of disks.");
+				}
+                else
+                {
+                    Tower(n-1, startRod, endRod, auxiliaryRod);
+                    Console.Write(startRod + " -> " + endRod);
+                    Tower(n-1, auxiliaryRod, startRod, endRod);
+                }
+            }
     }
 }
+
+
+
