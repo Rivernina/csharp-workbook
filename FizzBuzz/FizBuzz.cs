a370f51d818ea1bdbc35a93f44f950d7bd2a7150
Nina Riveron <Rivernina512@gmail.com>
2018-06-07 14:07:52 -0500

    FizzBuzz

---
+++ FizzBuzz/FizBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzz 
+{
+					
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("{0,70}", "*****************************************");
+			Console.WriteLine("\t\t\t\t\t\t\t *   \t\t\t\t\t\t\t\t\t*");
+			Console.WriteLine("\t\t\t\t\t\t\t *{0,34}",  "Welcome to FizzBuzz! \t\t *");
+			Console.WriteLine("\t\t\t\t\t\t\t *   \t\t\t\t\t\t\t\t\t*");
+
+			Console.WriteLine("{0,70}", "*****************************************");
+			Console.WriteLine("");
+			Console.WriteLine("");
+	 
+	
+        	for (int i = 0; i < 100; i++)		
+			if 	(i % 3 == 0 && i % 5 == 0)
+			{
+				Console.WriteLine("FizzBuzz");
+			}
+			else if (i % 3 == 0)
+			{
+				Console.WriteLine("Fizz");
+			}
+			else if (i % 5 == 0)
+			{
+				Console.WriteLine("Buzz");
+			}
+			else
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
